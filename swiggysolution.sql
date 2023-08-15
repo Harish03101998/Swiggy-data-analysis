b36ef72ef04a44401082a8b25ad11bb81955d61a
Harish <118190262+Harish03101998@users.noreply.github.com>
2023-08-15 20:29:44 +0530

    Update swiggysolution.sql

--- swiggysolution.sql
+++ swiggysolution.sql
@@ -29,38 +29,37 @@ limit 1;
 select city, avg(rating) as average_rating
 from swiggy group by city;
 
-#Q6 WHAT IS THE HIGHEST PRICE OF ITEM UNDER THE 'RECOMMENDED' MENU
-CATEGORY FOR EACH RESTAURANT
+#Q6 WHAT IS THE HIGHEST PRICE OF ITEM UNDER THE 'RECOMMENDED' MENU CATEGORY FOR EACH RESTAURANT?
 select distinct restaurant_name,
 menu_category,max(price) as highestprice
 from swiggy where menu_category='Recommended'
 group by restaurant_name,menu_category;
 
-#Q7
+#Q7 FIND THE TOP 5 MOST EXPENSIVE RESTAURANTS THAT OFFER CUISINE OTHER THAN INDIAN CUISINE
 select distinct restaurant_name,cost_per_person
 from swiggy where cuisine<>'Indian'
 order by cost_per_person desc
 limit 5;
 
-#Q8
+#Q8 FIND THE RESTAURANTS THAT HAVE AN AVERAGE COST WHICH IS HIGHER THAN THE TOTAL AVERAGE COST OF ALL RESTAURANTS TOGETHER.
 select distinct restaurant_name,cost_per_person
 from swiggy where cost_per_person>(
 select avg(cost_per_person) from swiggy);
 
-#Q9
+#Q9 RETRIEVE THE DETAILS OF RESTAURANTS THAT HAVE THE SAME NAME BUT ARE LOCATED IN DIFFERENT CITIES.
 select distinct t1.restaurant_name,t1.city,t2.city
 from swiggy t1 join swiggy t2 
 on t1.restaurant_name=t2.restaurant_name and
 t1.city<>t2.city;
 
-#Q10
+#Q10 WHICH RESTAURANT OFFERS THE MOST NUMBER OF ITEMS IN THE 'MAIN COURSE' CATEGORY?
 select distinct restaurant_name,menu_category
 ,count(item) as no_of_items from swiggy
 where menu_category='Main Course' 
 group by restaurant_name,menu_category
 order by no_of_items desc limit 1;
 
-#Q11
+#Q11 LIST THE NAMES OF RESTAURANTS THAT ARE 100% VEGEATARIAN IN ALPHABETICAL ORDER OF RESTAURANT NAME.
 select distinct restaurant_name,
 (count(case when veg_or_nonveg='Veg' then 1 end)*100/
 count(*)) as vegetarian_percetage
@@ -69,20 +68,20 @@ group by restaurant_name
 having vegetarian_percetage=100.00
 order by restaurant_name;
 
-#Q12
+#Q12 WHICH IS THE RESTAURANT PROVIDING THE LOWEST AVERAGE PRICE FOR ALL ITEMS?
 select distinct restaurant_name,
 avg(price) as average_price
 from swiggy group by restaurant_name
 order by average_price limit 1;
 
-#Q13
+#Q13 WHICH TOP 5 RESTAURANT OFFERS HIGHEST NUMBER OF CATEGORIES?
 select distinct restaurant_name,
 count(distinct menu_category) as no_of_categories
 from swiggy
 group by restaurant_name
 order by no_of_categories desc limit 5;
 
-#Q14
+#Q14 WHICH RESTAURANT PROVIDES THE HIGHEST PERCENTAGE OF NON-VEGEATARIAN FOOD?
 select distinct restaurant_name,
 (count(case when veg_or_nonveg='Non-veg' then 1 end)*100
 /count(*)) as nonvegetarian_percentage
